a30e6e07d54f39d7d5fc1982422d9213ed4b0f44
Ankur Pandey <ankurpandeyvns@gmail.com>
2017-07-07 06:05:51 +0530

    Build Error Fix!

--- ham/ham-vendor-blobs.mk
+++ ham/ham-vendor-blobs.mk
@@ -44,9 +44,7 @@ PRODUCT_COPY_FILES += \
     vendor/zuk/ham/proprietary/vendor/lib/librpmb.so:system/vendor/lib/librpmb.so \
     vendor/zuk/ham/proprietary/vendor/lib/libssd.so:system/vendor/lib/libssd.so \
     vendor/zuk/ham/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-    vendor/zuk/ham/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
     vendor/zuk/ham/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    vendor/zuk/ham/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/zuk/ham/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/zuk/ham/proprietary/bin/loc_launcher:system/bin/loc_launcher \
     vendor/zuk/ham/proprietary/bin/lowi-server:system/bin/lowi-server \
